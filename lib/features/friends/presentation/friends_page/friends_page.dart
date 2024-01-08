13c5c69421b0598737818497d0449d875460da84
Likssss <148707565+Likssss@users.noreply.github.com>
2024-01-08 19:11:51 +0700

    Update friends_page.dart

--- lib/features/friends/presentation/friends_page/friends_page.dart
+++ lib/features/friends/presentation/friends_page/friends_page.dart
@@ -3,10 +3,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:foodie/features/friends/presentation/chat_page/chat_controller.dart';
+import 'package:NOMNOM/features/friends/presentation/chat_page/chat_controller.dart';
 
-import 'package:foodie/features/friends/presentation/friends_page/widgets/friend_card.dart';
-import 'package:foodie/router/router_context_extension.dart';
+import 'package:NOMNOM/features/friends/presentation/friends_page/widgets/friend_card.dart';
+import 'package:NOMNOM/router/router_context_extension.dart';
 import 'package:intl/intl.dart';
 
 import '../../../../constants/firebase_constants.dart';
