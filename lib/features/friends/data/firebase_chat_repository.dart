493a95d72f68354e1f49bf1a10f9f6e28f2b8b8f
Likssss <148707565+Likssss@users.noreply.github.com>
2024-01-08 19:09:39 +0700

    Update firebase_chat_repository.dart

--- lib/features/friends/data/firebase_chat_repository.dart
+++ lib/features/friends/data/firebase_chat_repository.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:foodie/constants/firebase_constants.dart';
+import 'package:NOMNOM/constants/firebase_constants.dart';
 
 import '../domain/app_user.dart';
 import '../domain/chat_message.dart';
